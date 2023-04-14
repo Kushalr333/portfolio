ea0e8dbbf76281c9443defab4203b1f666ff8b44
Kushalr333 <107407779+Kushalr333@users.noreply.github.com>
2023-04-14 18:40:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section id="about">
+        <h2>About Me</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis metus ut nibh faucibus, in commodo felis viverra. Ut aliquam consequat velit, vitae euismod sapien porta sit amet. Vestibulum lobortis tristique metus, vel consectetur nisl pellentesque vitae.</p>
+      </section>
+      <section id="projects">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <h3>Project 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis metus ut nibh faucibus, in commodo felis viverra.</p>
+          </li>
+          <li>
+            <h3>Project 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis metus ut nibh faucibus, in commodo felis viverra.</p>
+          </li>
+          <li>
+            <h3>Project 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis metus ut nibh faucibus, in commodo felis viverra.</p>
+          </li>
+        </ul>
+      </section>
+      <section id="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <p>Contact Me:</p>
+      <ul>
+        <li>Email: john@example.com</li>
+        <li>Phone: 555-555-5555</li>
+      </ul>
+    </footer>
+  </body>
+</html>
